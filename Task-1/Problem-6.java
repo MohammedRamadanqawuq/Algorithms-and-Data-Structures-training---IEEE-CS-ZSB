43fba1557238dd605604940309d2f14485aafa78
Mohammed Ramadan <96293938+MohammedRamadanqawuq@users.noreply.github.com>
2022-02-24 18:24:20 +0200

    Problem-6

---
+++ Task-1/Problem-6.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class TheOutput {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        String word;
+        for (int i = 0; i < num; i++) {
+            word = input.next();
+        }
+        for (int i = 0; i < num; i++) {
+
+            System.out.println("gzuz");
+
+        }
+    }
+
+}
