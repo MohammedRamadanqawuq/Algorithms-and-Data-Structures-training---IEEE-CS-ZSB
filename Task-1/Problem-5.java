8530e3cfd7571f2e957c99c015c6e6a698d22e01
Mohammed Ramadan <96293938+MohammedRamadanqawuq@users.noreply.github.com>
2022-02-24 18:10:06 +0200

    Problem-5

---
+++ Task-1/Problem-5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        //System.out.print("Enter the number you want to in the multiplication table : ");
+        long number=s.nextLong();
+       //System.out.println("The multiplication table of "+number+" is : ");
+        for(int i=1;i<=12;i++){
+          long multiplicatino=number*i;
+          System.out.println(number+" "+"X"+" "+i+" "+"="+" "+multiplicatino);
+        }  
+    }
+    
+}
